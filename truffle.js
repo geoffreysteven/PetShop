a18b0051c61394152d448344239553cca3a4ce10
Geoffrey Shmigelsky <geoff@shmigelsky.com>
2017-11-28 21:32:23 -0800

    Change truffle port to 8545

--- truffle.js
+++ truffle.js
@@ -3,10 +3,8 @@ module.exports = {
   // to customize your Truffle configuration!
  networks: {
     development: {
-      // host: "192.168.86.3", // Laptop
-      // host: "192.168.86.4", // Windows
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*", // Match any network id
 //      gas: 6712390  // to mache the Ganache server
     }
